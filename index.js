13391bdecc2a40fc8fde05e746e722491274c9a4
VanithaN2007 <nv2318546@gmail.com>
2024-09-05 14:23:21 +0530

    modified 2

--- index.js
+++ index.js
@@ -1,21 +1,65 @@
-let a=101;
-let b=103;
-let c=105;
-a=a+a+a+a;
-b=4b-3b;
-c=c**2;
-console.log(a+b+c);
+// let a=101;
+// let b=103;
+// let c=105;
+// a=a+a+a+a;
+// b=4b-3b;
+// c=c**2;
+// console.log(a+b+c);
 
 
-let a=5;
-let b=2;
-let c=1;
-let d= (a+b+C);
-console.log(d);
+// let a=5;
+// let b=2;
+// let c=1;
+// let d= (a+b+C);
+// console.log(d);
 
 
 
+// let mark=100;
+// if (mark>=35){
+//     console.log("pass");
+// }
+// else {
+//     console.log("faill");
+// }
 
+// let mark = 60;
+// let grade;
+// if (mark >= 65) {
+//     console.log("firstclass");
+//     grade="a";
+// }
+// else if (mark >= 45) {
+//     console.log("secondclass");
+// }
+// else { console.log("faill"); }
+
+
+let letter = "a";
+switch (letter) {
+    case "a":
+        console.log("vowel");
+        break;
+    case "e":
+        console.log("vowel");
+        break;
+    case "i":
+        console.log("vowel");
+        break;
+    case "o":
+        console.log("vowel");
+        break;
+    case "u":
+        console.log("vowel");
+        break;
+}
+
+let a=2;
+let b=4;
+if (a >b){
+    console.log("a");
+}
+else {console.log("b");}
 
 
 
