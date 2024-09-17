acc8447a8eb9b7fc5dc5f141ec4b7e3b423b312a
VanithaN2007 <nv2318546@gmail.com>
2024-09-17 17:04:56 +0530

    modified the portfolio photo add

--- index.js
+++ index.js
@@ -1,68 +1,392 @@
-// let a=101;
-// let b=103;
-// let c=105;
-// a=a+a+a+a;
-// b=4b-3b;
-// c=c**2;
-// console.log(a+b+c);
+// // let a=101;
+// // let b=103;
+// // let c=105;
+// // a=a+a+a+a;
+// // b=4b-3b;
+// // c=c**2;
+// // console.log(a+b+c);
 
 
-// let a=5;
-// let b=2;
-// let c=1;
-// let d= (a+b+C);
-// console.log(d);
+// // let a=5;
+// // let b=2;
+// // let c=1;
+// // let d= (a+b+C);
+// // console.log(d);
 
 
 
-// let mark=100;
-// if (mark>=35){
-//     console.log("pass");
+// // let mark=100;
+// // if (mark>=35){
+// //     console.log("pass");
+// // }
+// // else {
+// //     console.log("faill");
+// // }
+
+// // let mark = 60;
+// // let grade;
+// // if (mark >= 65) {
+// //     console.log("firstclass");
+// //     grade="a";
+// // }
+// // else if (mark >= 45) {
+// //     console.log("secondclass");
+// // }
+// // else { console.log("faill"); }
+
+
+// let letter = "a";
+// switch (letter) {
+//     case "a":
+//         console.log("vowel");
+//         break;
+//     case "e":
+//         console.log("vowel");
+//         break;
+//     case "i":
+//         console.log("vowel");
+//         break;
+//     case "o":
+//         console.log("vowel");
+//         break;
+//     case "u":
+//         console.log("vowel");
+//         break;
+// }
+
+// let a=2;
+// let b=4;
+// if (a >b){
+//     console.log("a");
+// }
+// else {console.log("b");}
+
+
+
+// let mark=60;
+// if(mark==60){
+//     console.log(equal)
+// }
+// else{
+//     console.log(not equal)
+// }
+
+
+// let count=1;
+// while(count<10){
+//     console.log("the value of count is"+count);
+//     count++;
+// }
+
+// let rollno=1001;
+// while(rollno<1028){
+//     console.log("r.no"+rollno);
+//     rollno++;
+// }
+
+// let num=1;
+// while(num<11){
+//     console.log(3*num);
+//     num++;
+// }
+
+// let num=1;
+// while(num<12){
+//     console.log(2*num);
+//     num--;
+// }
+
+// let number=1;
+// while(number<10){
+//     console.log(2*number);
+//     number++;
+// }
+
+// function printtable(num){
+//       console.log(2*num);
+//       num++;
+// }
+
+// printtable(2,15);
+// function printtable(tablevalue,number){
+//     console.log("The value of table"+tablevalue);
+//     console.log(number);
+//     let count=1;
+//     while(count<number){
+//         console.log(tablevalue+"*"+count+"="+tablevalue*count);
+//         count++;
+//     }
+// }
+
+// let num=1;
+// while(num<=5){
+//     console.log("The value of num before incrementing is " + num);
+//     num++;
+//     console.log("The value of num after incrementing is " + num);
+    
+// }
+
+// // let i=1;
+// // do{
+// //     console.log("time")
+// // }
+// // while(10>i){
+// //     i++;
+// // }
+
+// // let a=1;
+// // do{
+// //     console.log("small")
+// // }
+// // while(20>a){
+// //     a++;
+// // }
+
+// let k=1;
+// do{ 
+//     console.log("condition")
+// }
+// while(10>k){
+//     k++;
+// }
+
+
+
+
+
+// let (10,20)
+// funtion printtable(a,b){
+//     console.log()
+
+// }
+
+// console.log("tryprogramiz.pro");
+// // console.log("value of added numbers===>"+addtwonumbers(10,20))
+// var firsttwonumbers = addtwonumbers(10,20);
+// console.log(firsttwonumbers);
+// function addtwonumbrs(a,b){
+//     return a+b;
+// }
+
+
+// console.log(add(33,44))
+// console.log(sub(33,44))
+// console.log(mut(33,44))
+// console.log(div(33,44))
+// function add(a,b){
+//     return a+b;
+// }
+// function sub(a,b){
+//     return a-b;
+// }
+// function mut(a,b){
+//     return a*b;
+// }
+// function div(a,b){
+//     return a/b;
+// }
+
+
+// console.log("i am"+print("vani"));
+// console.log(rollno(2007))
+// console.log(class+print1(fssa))
+// funtion print(value){
+//     return value;
+// }
+// funtion print1(value){
+//     return value;
+// }
+
+// let a=-5;
+// function vanitha(){
+//     while(a<6){
+//         console.log(a)
+//             a++;
+//         }
+// }
+// console.log(vanitha());
+
+// let a=30;
+// function vani(){
+//     while(a<=0){
+//        console.log(a)
+//        a--;
+//     }
+// }
+// console.log(vani());
+
+
+// let a=2;
+// while (a<=50){
+//     console.log(a)
+//     a=a+2;
+// }
+
+// let a=1;
+// while (a<=50){
+//     console.log(a)
+//     a=a+2;
+// }
+
+// console.log(num(10))
+// function sum(n){
+//     let sum=10;
+//     let i=20;
+//     while(i<n){
+//         sum+=i;
+//         i++;
+//     }
+//     return sum;
+// }
+
+// console.log(number(5))
+// function number(n){
+//     let sum=0;
+//     let i=1;
+// while(i<=n){
+//     sum+=i;
+//     i++;
+// }
+// return sum;
+// }
+
+// console.log(num(5))
+// function num(n){
+//     let sum=0;
+//     let a=1;
+// while(a<=n){
+//         sum*=a;
+//         a++;
+//  }
+// return sum;
+// }
+
+
+
+// for(x=1;x<=5;x++){
+//     console.log(x);
+// }
+
+// for(x=5;x>=1;x--){
+//     console.log(x);
+// }
+
+// console.log(add(4))
+// function add(x){
+//     x=x+2;
+//     while "hi"+x;
+// }
+
+
+
+
+
+// console.log(num(20))
+// function num(n){
+//     let sum=0;
+//     let i=1;
+//     while(a<=1){
+//         num( )
+//     }
+// }
+
+// let result=" ";
+// let a=*;
+// while(a<=5){
+//     result=result+a+" ";
+//     a++;
+//     console.log(result);
+// }
+
+
+// console.log (even or odd(35));
+// if (n%<==50){
+//     return even;
 // }
 // else {
-//     console.log("faill");
+//     return odd;
 // }
 
-// let mark = 60;
-// let grade;
-// if (mark >= 65) {
-//     console.log("firstclass");
-//     grade="a";
-// }
-// else if (mark >= 45) {
-//     console.log("secondclass");
-// }
-// else { console.log("faill"); }
+// printmultiplicationtable(5,6)
+//     function printmultiplicationtable(m,r){
+//         let i =0;
+//         while(1<r){
+//             console.log(r+"x"+m+"="+m*r)
+//             r--;
+//         }
+//     }
 
 
-let letter = "a";
-switch (letter) {
-    case "a":
-        console.log("vowel");
-        break;
-    case "e":
-        console.log("vowel");
-        break;
-    case "i":
-        console.log("vowel");
-        break;
-    case "o":
-        console.log("vowel");
-        break;
-    case "u":
-        console.log("vowel");
-        break;
-}
+//  getdayname(10)
+//  function getdayname(i){
+//     switch(i){
+//         case 1:
+//             console.log("sunday")
+//             break;
+//         case 2:
+//             console.log("monday")
+//             break;
+//         case 3:
+//             console.log("tuesday")
+//             break;
+//         case 4:
+//             console.log("wednesday")
+//             break;
+//         case 5:
+//             console.log("thursday")
+//             break;
+//         case 6:
+//             console.log("friday")
+//             break;
+//         case 7:
+//             console.log("saturday")
+//             break;
+//         default:
+//             console.log("inisal date")   
+//     }
+//  }
+
+//  printtable(5,6)
+//  function printtable(r,n){
+//     i=0;
+//     while(1<n){
+//         console.log(r+"x"+n+"="+r*n)
+//         n--;
+//     }
+//  }
+
+// let a=0;
+// let b=10;
+// while(b>a){
+//     console.log(b)
+//     b++;
+// }
+
+// let fruit = ["apple","orange","lemon"]
+// console.log("value of cars 3 is ------>"+fruit[3]);
+// console.log("")
+// for (i=0; i<fruit.length;i++){
+//     console.log(fruit[i]);
+// }
+
+// let names = ["hema","afreen","vanitha"]
+// console.log("value of names 3 is--->"+names[3]);
+// console.log("")
+// for (i=3; i>=0;i--){
+//     console.log(names[i]);
+// }
+
 
 let a=2;
-let b=4;
-if (a >b){
-    console.log("a");
+while (a<=5){
+    console.log(a)
+    a=a+2;
 }
-else {console.log("b");}
 
-
-
-
-
-
+function greet(name){
+    console.log('helo,$(namne)'):
+}
+ function greetgoodafternoon(name){
+    console.log('good afternoon,$(name)')
+ }
+ 
